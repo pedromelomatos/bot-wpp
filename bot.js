90e11542ad8d8b4bb570bbfba015228682754b8c
Pedro Henrique Melo Matos <melomatosp@gmail.com>
2025-07-30 13:00:14 -0300

    base do bot criado

---
+++ bot.js
@@ -0,0 +1,21 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client();
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('message_create', message => {
+	if (message.body === 'Bom dia.') {
+
+		client.sendMessage(message.from, 'Olá!');
+	}
+});
+
+client.initialize();
