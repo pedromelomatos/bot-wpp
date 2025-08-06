3ff7d8742d0ecde5d36010a4f7c9c5e12133e264
Pedro Henrique Melo Matos <melomatosp@gmail.com>
2025-08-05 23:05:14 -0300

    correções de bug
    bot estava entrando em um auto loop de só responder mensagens que ele mesmo enviou

--- bot.js
+++ bot.js
@@ -13,36 +13,39 @@ client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
 });
 
-client.on('message_create', message => {
+client.on('message_create', async message => {
+
+    if (message.fromMe) {
+        return;
+    }
+
 	if (message.body === 'Bom dia.') {
 
-		client.sendMessage(message.from, 'Olá!');
+		await client.sendMessage(message.from, 'Olá!');
+        return
 	}
-});
-
-client.on('message_create', message => {
     const numero_usuario = message.from
     const agora = Date.now()
     const ultimo_atendimento = horarios_e_atendidos.get(numero_usuario) //fazendo um get no map de horarios e atendidos pra ver se esse usuario já foi atendido e qual foi o horário
     if (!ultimo_atendimento || agora - ultimo_atendimento > 86400000)//se esse usuario nunca foi atendido ou se já se passou mais de um dia desde que ele foi atendido:
     {
         horarios_e_atendidos.set(numero_usuario, agora) //adicionando o usuario e o horario de atendimento dele
-        client.sendMessage(message.from, 'Olá! Seja bem-vindo à clínica. Digite:\n1 - Falar com atendente\n2 - Saber mais sobre a clínica')
+        await client.sendMessage(message.from, 'Olá! Seja bem-vindo à clínica. Digite:\n1 - Falar com atendente\n2 - Saber mais sobre a clínica')
         return
     } 
     
     if(message.body === '1'){
-        client.sendMessage(message.from, "👩‍💼 Vamos chamar um atendente para você.")
-        client.sendMessage(numero_atendente, `📞 O número ${numero_usuario} está solicitando atendimento.`)
+        await client.sendMessage(message.from, "👩‍💼 Vamos chamar um atendente para você.")
+        await client.sendMessage(numero_atendente, `📞 O número ${numero_usuario} está solicitando atendimento.`)
 
     }
 
-	if (message.body === '2') {
+	else if (message.body === '2') {
 
-		client.sendMessage(message.from, "🏥 Sobre a clínica:\n\n- Tratamentos: Psicoterapia Infantil, Psicopedagogia Fonoaudiologia e Nutrição Comportamental.\n- Localização: Santana, São Paulo (SP).\n- Site: https://deupositivo.com.br\n\n");
+		await client.sendMessage(message.from, "🏥 Sobre a clínica:\n\n- Tratamentos: Psicoterapia Infantil, Psicopedagogia Fonoaudiologia e Nutrição Comportamental.\n- Localização: Santana, São Paulo (SP).\n- Site: https://deupositivo.com.br\n\n");
 	}
     else{
-        client.sendMessage("🤔 O que você gostaria?\nDigite:\n1 - Falar com atendente\n2 - Saber mais sobre a clínica'")
+        await client.sendMessage(message.from, "🤔 O que você gostaria?\nDigite:\n1 - Falar com atendente\n2 - Saber mais sobre a clínica")
     }
 });
 
